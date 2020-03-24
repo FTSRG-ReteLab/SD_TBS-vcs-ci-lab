7e232de8a4911c4da1bc6b516ba7816c13ce32b7
david <sandordavid1124@gmail.com>
2020-03-24 14:36:01 +0100

    joystick feature implemented

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -3,12 +3,14 @@ package hu.bme.mit.train.controller;
 import hu.bme.mit.train.interfaces.TrainController;
 import com.google.common.collect.*;
 import java.lang.*;
-public class TrainControllerImpl implements TrainController {
+public class TrainControllerImpl extends Thread implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
 	private Table<Long, Integer, Integer> table = new ImmutableTable.Builder<Long, Integer, Integer>().put(System.currentTimeMillis(), step, referenceSpeed).build();
+
+
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
@@ -46,6 +48,18 @@ public class TrainControllerImpl implements TrainController {
 	public void setJoystickPosition(int joystickPosition) {
 		this.step = joystickPosition;		
 	}
-	
+
 	public Table<Long,Integer,Integer> getTachometer(){return table;}
+
+	@Override
+	public void run() {
+		while (true){
+			try {
+				followSpeed();
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
