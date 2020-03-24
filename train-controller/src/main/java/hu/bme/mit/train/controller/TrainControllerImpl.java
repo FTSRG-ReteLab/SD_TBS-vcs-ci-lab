a9a98b853f9c12a5240b8ac5bf1c4419296a56fa
david <sandordavid1124@gmail.com>
2020-03-24 14:54:16 +0100

    formatted the source code

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,64 +2,66 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 import com.google.common.collect.*;
+
 import java.lang.*;
+
 public class TrainControllerImpl extends Thread implements TrainController {
 
-	private int step = 0;
-	private int referenceSpeed = 0;
-	private int speedLimit = 0;
-	private Table<Long, Integer, Integer> table = new ImmutableTable.Builder<Long, Integer, Integer>().put(System.currentTimeMillis(), step, referenceSpeed).build();
-
-
-	@Override
-	public void followSpeed() {
-		if (referenceSpeed < 0) {
-			referenceSpeed = 0;
-		} else {
-		    if(referenceSpeed+step > 0) {
+    private int step = 0;
+    private int referenceSpeed = 0;
+    private int speedLimit = 0;
+    private Table<Long, Integer, Integer> table = new ImmutableTable.Builder<Long, Integer, Integer>().put(System.currentTimeMillis(), step, referenceSpeed).build();
+    @Override
+    public void followSpeed() {
+        if (referenceSpeed < 0) {
+            referenceSpeed = 0;
+        } else {
+            if (referenceSpeed + step > 0) {
                 referenceSpeed += step;
             } else {
-		        referenceSpeed = 0;
+                referenceSpeed = 0;
             }
-		}
+        }
 
-		enforceSpeedLimit();
-	}
+        enforceSpeedLimit();
+    }
 
-	@Override
-	public int getReferenceSpeed() {
-		return referenceSpeed;
-	}
+    @Override
+    public int getReferenceSpeed() {
+        return referenceSpeed;
+    }
 
-	@Override
-	public void setSpeedLimit(int speedLimit) {
-		this.speedLimit = speedLimit;
-		enforceSpeedLimit();
-		
-	}
+    @Override
+    public void setSpeedLimit(int speedLimit) {
+        this.speedLimit = speedLimit;
+        enforceSpeedLimit();
 
-	private void enforceSpeedLimit() {
-		if (referenceSpeed > speedLimit) {
-			referenceSpeed = speedLimit;
-		}
-	}
+    }
 
-	@Override
-	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
-	}
+    private void enforceSpeedLimit() {
+        if (referenceSpeed > speedLimit) {
+            referenceSpeed = speedLimit;
+        }
+    }
 
-	public Table<Long,Integer,Integer> getTachometer(){return table;}
+    @Override
+    public void setJoystickPosition(int joystickPosition) {
+        this.step = joystickPosition;
+    }
 
-	@Override
-	public void run() {
-		while (true){
-			try {
-				followSpeed();
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public Table<Long, Integer, Integer> getTachometer() {
+        return table;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                followSpeed();
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
