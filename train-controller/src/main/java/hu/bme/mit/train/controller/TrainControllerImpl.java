cd13dc73b1142541a06cd084fa511d1afdee3dea
Stella Tóth-Baranyi <stellasipi@gmail.com>
2020-04-02 00:34:04 +0200

    Fix vulnerability

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -3,12 +3,16 @@ package hu.bme.mit.train.controller;
 import hu.bme.mit.train.interfaces.TrainController;
 import com.google.common.collect.*;
 import java.lang.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
 	private Table<Long, Integer, Integer> table = new ImmutableTable.Builder<Long, Integer, Integer>().put(System.currentTimeMillis(), step, referenceSpeed).build();
 	private Thread thread;
+	Logger logger = Logger.getLogger(TrainControllerImpl.class.getName());
 
 	public TrainControllerImpl(){
 		thread=new Thread() {
@@ -18,7 +22,7 @@ public class TrainControllerImpl implements TrainController {
 					followSpeed();
 					thread.sleep(1000);
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					logger.log(Level.SEVERE,"Got InterruptedException");
 				}
 
 			}
